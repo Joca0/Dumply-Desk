d782aafd7e50130b42e9a9a69f298d3de31f549b
Joca0 <137853850+Joca0@users.noreply.github.com>
2026-05-24 02:18:51 -0300

    chore: spring initializr

---
+++ dumplydesk/src/test/java/com/dumplydesk/DumplydeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dumplydesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumplydeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
